d5467ae9f71bb43230119bfb493feda61594c87e
Jithendra <jithendraediga6@gmail.com>
2023-10-12 16:18:57 +0530

    First commit

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10, b = 20, add;
+        add = a + b;
+        System.out.println("a =" +a);
+        System.out.println("b =" +b);
+        System.out.println("add =" +add);
+    }
+}
---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a = 100, b = 20, sub;
+        sub = a - b;
+        System.out.println("a =" +a);
+        System.out.println("b =" +b);
+        System.out.println("sub =" +sub);
+    }
+}
